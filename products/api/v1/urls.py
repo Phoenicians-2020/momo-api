f7a083f49208f9cb5e003280a366704cf75cb7ae
James Joshua Ginete <joshua@ingenuity.ph>
2020-10-08 17:53:50 +0800

    Modify the url configurations for api v1

--- products/api/v1/urls.py
+++ products/api/v1/urls.py
@@ -3,10 +3,9 @@ from rest_framework import routers
 
 from products.api.v1 import viewsets
 
-router = routers.DefaultRouter()
-router.register('products', viewsets.ProductsViewSet, basename='products')
+router = routers.DefaultRouter(trailing_slash=False)
+router.register('', viewsets.ProductsViewSet, basename='search-products')
 router.register('product-types', viewsets.ProductTypesViewSet, basename='product-types')
-router.register('filter-products', viewsets.FilterProductsViewSet, basename='filter-products')
 
 urlpatterns = [
     path("", include(router.urls)),

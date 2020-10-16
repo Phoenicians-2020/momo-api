c4ed993266bca74d063a9327b15d81003c9004e1
James Joshua Ginete <joshua@ingenuity.ph>
2020-10-16 21:08:57 +0800

    Add users django app

---
+++ users/forms.py
@@ -0,0 +1,18 @@
+# from django.contrib.auth.forms import UserCreationForm
+
+# from users.models import User
+
+
+# class UserCreateForm(UserCreationForm):
+
+#     class Meta:
+#         model = User
+#         fields = (
+#             'first_name',
+#             'last_name',
+#             'phone_number',
+#             'username',
+#             'email',
+#             'password',
+#             'auth_group',
+#         )

d2bd5398c7de2c5896980cbdfa9c5e999ebf1006
Diomari Madulara <diomari@rev365.com>
2020-11-05 15:38:43 +0800

    Add url router for sellers endpoint

--- home/api/v1/urls.py
+++ home/api/v1/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('products/', include('products.api.v1.urls'))
+    path('products/', include('products.api.v1.urls')),
+    path('sellers/', include('users.api.v1.urls'))
 ]

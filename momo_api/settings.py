a167b75f58426c869d0dc27affcb1b9452bcc855
Diomari Madulara <diomari@rev365.com>
2020-11-11 15:31:35 +0800

    Minimal fixes in STATICFILES_DIRS

--- momo_api/settings.py
+++ momo_api/settings.py
@@ -154,7 +154,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'assets/'),
+    os.path.join(BASE_DIR, 'static/'),
 ]
 
 django_heroku.settings(locals())
